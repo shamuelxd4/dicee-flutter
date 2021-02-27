a6894c8a4ff0a3b6baeb520ed42f9df719d5a7d6
Samuel Quenta Carvajal <50058913+shamuelxd4@users.noreply.github.com>
2021-02-26 20:52:58 -0400

    optimizando el codigo de la app

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blueGrey.shade700,
         appBar: AppBar(
-          title: Text('JUEGO DE DADOS'),
-          backgroundColor: Colors.red,
+          title: Center(
+            child: Text("JUEGO DE DADOS"),
+          ),
+          backgroundColor: Colors.blueGrey.shade800,
         ),
         body: DicePage(),
       ),
@@ -16,9 +19,54 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rigthDiceNumber = 1;
+
+  void dados() {
+    setState(() {
+      leftDiceNumber = (Random().nextInt(6)) + 1;
+      rigthDiceNumber = (Random().nextInt(6)) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Center(
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: boton(leftDiceNumber),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: boton(rigthDiceNumber),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  FlatButton boton(int posicion) {
+    return FlatButton(
+      padding: EdgeInsets.all(0),
+      color: Colors.orange.shade900,
+      onPressed: () {
+        dados();
+      },
+      child: Image.asset('images/dice$posicion.png'),
+    );
   }
 }
